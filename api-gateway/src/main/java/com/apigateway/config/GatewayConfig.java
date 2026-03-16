66076253100b1b7d79309519aeba9299b7be8dcb
GloryMs <majdsalman22@gmail.com>
2026-03-16 12:50:51 +0300

    Dashboard updates for both Patient and Provider.

--- api-gateway/src/main/java/com/apigateway/config/GatewayConfig.java
+++ api-gateway/src/main/java/com/apigateway/config/GatewayConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 public class GatewayConfig {
@@ -15,6 +16,11 @@ public class GatewayConfig {
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
         corsConfig.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+        corsConfig.setAllowedOrigins(List.of(
+                "http://localhost:3000",
+                "http://localhost:5173",
+                "http://localhost:5174"
+        ));
         corsConfig.setMaxAge(3600L);
         corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         corsConfig.setAllowedHeaders(Arrays.asList("*"));
